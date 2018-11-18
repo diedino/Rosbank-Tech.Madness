22cc802bded35e407cee38560ab79454a97eff08
Vasiliy <vvkrasnov@edu.hse.ru>
2018-11-18 09:45:33 +0300

    update statictic activity

--- app/src/main/java/ros/rosbank/Documents.java
+++ app/src/main/java/ros/rosbank/Documents.java
@@ -12,7 +12,14 @@ import android.view.MenuItem;
 import android.widget.TextView;
 
 import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.PieChart;
+import org.eazegraph.lib.charts.StackedBarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
 import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.PieModel;
+import org.eazegraph.lib.models.StackedBarModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 
 public class Documents extends AppCompatActivity {
@@ -32,16 +39,55 @@ public class Documents extends AppCompatActivity {
 
         BarChart mBarChart = (BarChart) findViewById(R.id.barchart);
 
-        mBarChart.addBar(new BarModel(2.3f, 0xFF123456));
-        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart.addBar(new BarModel(3.3f, 0xFF563456));
-        mBarChart.addBar(new BarModel(1.1f, 0xFF873F56));
-        mBarChart.addBar(new BarModel(2.7f, 0xFF56B7F1));
-        mBarChart.addBar(new BarModel(2.f,  0xFF343456));
-        mBarChart.addBar(new BarModel(0.4f, 0xFF1FF4AC));
-        mBarChart.addBar(new BarModel(4.f,  0xFF1BA4E6));
+        mBarChart.addBar(new BarModel(1f, 0xFF123456));
+        mBarChart.addBar(new BarModel(2f,  0xFF343456));
+        mBarChart.addBar(new BarModel(3f, 0xFF563456));
+        mBarChart.addBar(new BarModel(4f, 0xFF873F56));
+        mBarChart.addBar(new BarModel(5f, 0xFF56B7F1));
+        mBarChart.addBar(new BarModel(6f,  0xFF343456));
+        mBarChart.addBar(new BarModel(7f, 0xFF1FF4AC));
+        mBarChart.addBar(new BarModel(8f,  0xFF1BA4E6));
 
         mBarChart.startAnimation();
+
+        PieChart mPieChart = (PieChart) findViewById(R.id.piechart);
+
+        mPieChart.addPieSlice(new PieModel("Business", 40, Color.parseColor("#FE6DA8")));
+        mPieChart.addPieSlice(new PieModel("Mortage", 25, Color.parseColor("#56B7F1")));
+        mPieChart.addPieSlice(new PieModel("Consumer Loan", 35, Color.parseColor("#CDA67F")));
+
+        mPieChart.startAnimation();
+
+        StackedBarChart mStackedBarChart = (StackedBarChart) findViewById(R.id.stackedbarchart);
+
+        StackedBarModel s1 = new StackedBarModel("12.4");
+
+        s1.addBar(new BarModel(5f, 0xFF63CBB0));
+        s1.addBar(new BarModel(6f, 0xFF56B7F1));
+        s1.addBar(new BarModel(2f, 0xFFCDA67F));
+
+        StackedBarModel s2 = new StackedBarModel("13.4");
+        s2.addBar(new BarModel(5f, 0xFF63CBB0));
+        s2.addBar(new BarModel(2f, 0xFF56B7F1));
+        s2.addBar(new BarModel(6f, 0xFFCDA67F));
+
+        StackedBarModel s3 = new StackedBarModel("14.4");
+
+        s3.addBar(new BarModel(7f, 0xFF63CBB0));
+        s3.addBar(new BarModel(6f, 0xFF56B7F1));
+        s3.addBar(new BarModel(3f, 0xFFCDA67F));
+
+        StackedBarModel s4 = new StackedBarModel("15.4");
+        s4.addBar(new BarModel(4f, 0xFF63CBB0));
+        s4.addBar(new BarModel(3f, 0xFF56B7F1));
+        s4.addBar(new BarModel(4f, 0xFFCDA67F));
+
+        mStackedBarChart.addBar(s1);
+        mStackedBarChart.addBar(s2);
+        mStackedBarChart.addBar(s3);
+        mStackedBarChart.addBar(s4);
+
+        mStackedBarChart.startAnimation();
 
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigationdocs);
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.navigationdocs);
